658a6b915b08839087bb0d49fe1007cad62e7f7c
swirlycone <aceliang2001@gmail.com>
2020-07-25 12:52:07 -0400

    Created the space to put the menu

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,62 @@
+import game as game
 import pygame
 
+# import pygame.locals
+from pygame.locals import (
+    MOUSEBUTTONDOWN,
+    QUIT
+)
+
 pygame.init()
+
+# set up the display
+# screen = pygame.display.set_mode([1000, 700])
+
+# control variable for game loop
+running = True
+# vars for mouse click
+LEFT = 1
+RIGHT = 3
+
+
+def mode_generator(mode):
+    global screen
+    if mode == 'easy':
+        # 10 by 8 squares: each square is 75 x 75
+        length = 750
+        width = 600
+        screen = pygame.display.set_mode([length, width])
+        screen.fill((255, 255, 255))
+        screen.blit(set_up_tool_bar(length), [0, 0])
+    if mode == 'normal':
+        # 18 x 14
+    pygame.display.flip()
+
+
+def set_up_tool_bar(length):
+    # tool_bar is 75 in height
+    tool_bar = pygame.Surface([length, 75])
+    tool_bar.fill([0, 0, 0])
+    return tool_bar
+
+
+def main_loop():
+    global running
+    # game loop
+    # default easy mode, but changes if the mode is switched
+    mode_generator('easy')
+    while running:
+        ev = pygame.event.get()
+        for event in ev:
+            pos = pygame.mouse.get_pos()
+            if event.type == QUIT:
+                running = False
+            elif event.type == MOUSEBUTTONDOWN and event.button == LEFT:
+                pygame.draw.circle(screen, [0, 0, 255], pos, 3)
+            elif event.type == MOUSEBUTTONDOWN and event.button == RIGHT:
+                pygame.draw.circle(screen, [255, 255, 255], pos, 3)
+        pygame.display.flip()
+
+
+main_loop()
+pygame.quit()
