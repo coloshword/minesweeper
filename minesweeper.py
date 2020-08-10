3911d9142df43af8d5b0fd88a65a07db33ac77e0
swirlycone <aceliang2001@gmail.com>
2020-08-10 17:13:15 -0400

    Number of bombs now displayed when clicking tiles after the first click

--- minesweeper.py
+++ minesweeper.py
@@ -52,6 +52,11 @@ class Tile(pygame.sprite.Sprite):
         screen.blit(self.tile, [x, y])
         self.pressed = True
 
+    def display_numb(self):
+        display_numb = font.render(str(self.number), False, [255, 255, 255])
+        screen.blit(display_numb, (int(self.x + square_length / 2.5), int(self.y + square_length / 3)))
+        pygame.display.flip()
+
     def become_bomb(self):
         self.bomb = True
 
@@ -152,6 +157,7 @@ def change_tile_color(list_tiles, mouse_position, s_l):
             running = False
         else:
             tile_pressed.get_pressed(tile_pressed.x, tile_pressed.y)
+            tile_pressed.display_numb()
             pygame.display.flip()
             return loc_tile_pressed
 
